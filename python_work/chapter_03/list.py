19cba15fde47a9e7155fb2eae2f496beb323a006
Damien Baubriau <dam.baubriau@gmail.com>
2025-02-12 15:45:25 +0000

    add list.py to chapter_03

---
+++ python_work/chapter_03/list.py
@@ -0,0 +1,15 @@
+baubriaus = ["Damien", "Victoria", "Ruby", "Elea"]
+
+print(baubriaus)
+
+# ========== ACCESS ELEMENT IN A LIST ==========
+print(baubriaus[0])
+print(baubriaus[1])
+print(baubriaus[2])
+print(baubriaus[3])
+
+# ========== ACCESS ELEMENTS FORM THE END OF A LIST ==========
+print(baubriaus[-1])
+print(baubriaus[-2])
+print(baubriaus[-3])
+print(baubriaus[-4])
