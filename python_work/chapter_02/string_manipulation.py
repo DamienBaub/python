d9b2a991eee7bcd1c96007931802b5e72be4b771
Damien Baubriau <dam.baubriau@gmail.com>
2025-02-12 14:08:20 +0000

    add string_manipulation.py to chapter_02

---
+++ python_work/chapter_02/string_manipulation.py
@@ -0,0 +1,16 @@
+# ========== TAB IN STRING ==========
+print("\tPython")
+
+# ========== NEWLINE IN STRING ==========
+print("\nPython")
+
+# ========== REMOVE WHITESPACE ON THE RIGHT SIDE OF A STRING ==========
+name = "Damien Baubriau "
+print(name.rstrip())
+
+# ========== REMOVE WHITESPACE ON THE LEFT SIDE OF A STRING ==========
+print(name.lstrip())
+
+# ========== REMOVE PREFIXE ==========
+url = "https://nostarch.com"
+print(url.removeprefix('https://'))
