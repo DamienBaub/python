29227fe4524799d2f3f4342bfa91727e24ea3dea
Damien Baubriau <dam.baubriau@gmail.com>
2025-02-12 14:11:52 +0000

    add apostrophe.py to chapter_02

---
+++ python_work/chapter_02/apostrophe.py
@@ -0,0 +1,7 @@
+# ========== DOUBLE OR SINGLE QUOTE ==========
+
+message = "One of Python's strengths is its diverse community."
+
+# This will produce an error
+# message = 'One of Python\'s strengths is its diverse community.'
+print(message)
