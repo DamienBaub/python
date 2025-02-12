e71de9fd12530318c4d5ea0f556c61f5d637d960
Damien Baubriau <dam.baubriau@gmail.com>
2025-02-12 15:18:31 +0000

    add exercise_03 to chapter_02

---
+++ python_work/chapter_02/exercises/exercise_03.py
@@ -0,0 +1,9 @@
+# ========== NUMBER EIGHT ==========
+print(9 - 1)
+print(4 + 4)
+print(4 * 2)
+print(int(16 / 2)) # convert float to integer
+
+# ========== FAVORITE NUMBER ==========
+favorite_number = 6
+print(f"My favorite number is {favorite_number}.")
