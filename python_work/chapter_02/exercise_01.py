e4a016c8362972e3a6b9dbf04437740dc78ff843
Damien Baubriau <dam.baubriau@gmail.com>
2025-02-12 11:33:57 +0000

    add exercise_01 to chapter_02

---
+++ python_work/chapter_02/exercise_01.py
@@ -0,0 +1,9 @@
+# ========== SIMPLE MESSAGE ==========
+
+message = "Hello, my name is Damien"
+print(message)
+
+# ========== SIMPLE MESSAGES ==========
+
+message = "Sorry, my name is Victoria"
+print(message)
