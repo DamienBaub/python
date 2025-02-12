77d4a48af3dfbf4a990c0ed058ff7272f75051df
Damien Baubriau <dam.baubriau@gmail.com>
2025-02-12 11:45:04 +0000

    add name.py to chapter_02

---
+++ python_work/chapter_02/name.py
@@ -0,0 +1,10 @@
+# ========== METHOD TO CHANGE EACH WORD TO TITLE CASE ==========
+
+name = "damien baubriau"
+print(name.title())
+
+# ========== METHOD TO UPPERCASE OR LOWERCASE ==========
+
+print(name.upper())
+print(name.lower())
+
