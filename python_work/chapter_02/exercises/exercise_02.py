4b33e3dd4ef1cb30ad6b9ce05eccf794ce9deb7e
Damien Baubriau <dam.baubriau@gmail.com>
2025-02-12 14:35:00 +0000

    add exercise_02 to chapter_02

---
+++ python_work/chapter_02/exercises/exercise_02.py
@@ -0,0 +1,34 @@
+# ========== PERSONAL MESSAGE ==========
+name = "Damien Baubriau"
+# print(f"Hello {name}, would you like to learn Python today?")
+
+# ========== NAME CASES ==========
+# print(name.lower())
+# print(name.upper())
+# print(name.title())
+
+# ========== FAMOUS QUOTE ==========
+# print('Albert Einstein once said, "A person who never made a mistake never tried anything new"')
+
+# ========== FAMOUS QUOTE 2 ==========
+# famous_person = "Albert Einstein"
+# message = f'{famous_person} once said, "A person who never made a mistake never tried anything new"'
+# print(message)
+
+# ========== STRIPPING NAMES ==========
+# name2 = "\tDamien BAUBRIAU\n"
+# print("Unmodified:")
+# print(name)
+#
+# print("\nUsing lstrip():")
+# print(name.lstrip())
+#
+# print("\nUsing rstrip():")
+# print(name.rstrip())
+#
+# print("\nUsing strip():")
+# print(name.strip())
+
+# ========== FILE EXTENSIONS ==========
+# filename = "python_notes.txt"
+# print(filename.removesuffix(".txt"))
