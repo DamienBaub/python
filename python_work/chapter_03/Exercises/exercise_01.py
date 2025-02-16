a7928becadc34587572921611b605d4968fcc05d
Damien Baubriau <dam.baubriau@gmail.com>
2025-02-16 10:51:32 +0000

    add exercise_01 to chapter_03

---
+++ python_work/chapter_03/Exercises/exercise_01.py
@@ -0,0 +1,10 @@
+names = ["Victoria", "Ruby", "Elea", "Adam", "Lucie"]
+for each in names:
+    print(each)
+
+for each in names:
+    print(f"Hello {each} how are you?")
+
+vehiculs = ["Tesla", "Mustang", "FatBoy"]
+for each in vehiculs:
+    print(f"I would love to have a {each}")
